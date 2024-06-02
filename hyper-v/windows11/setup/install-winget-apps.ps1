53830df9fd0e3f9577890641ff931622ab9e6fb4
Gabor Nemeth <etgabo@gmail.com>
2024-06-02 15:42:57 +0100

    fixed winget path

--- hyper-v/windows11/setup/install-winget-apps.ps1
+++ hyper-v/windows11/setup/install-winget-apps.ps1
@@ -1,5 +1,5 @@
 $winget_path = ls "C:\Program Files\windowsapps\Microsoft.DesktopAppInstaller*x64*8wekyb3d8bbwe" | select -ExpandProperty fullname
 
-& $asd\winget.exe install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
+& $winget_path\winget.exe install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 
-& $asd\winget.exe install Valve.Steam --silent --accept-package-agreements --accept-source-agreements
+& $winget_path\winget.exe install Valve.Steam --silent --accept-package-agreements --accept-source-agreements
